a0366874e33fb18fdac67180fb99d0b1258adad2
sanskarchoudhry <sanskar22101@gmail.com>
2025-01-06 21:45:26 +0530

    chore: add validation signup form

--- src/app/(onboarding)/(sign-up)/sign-up/_components/SignUpFormWrapper.tsx
+++ src/app/(onboarding)/(sign-up)/sign-up/_components/SignUpFormWrapper.tsx
@@ -1,18 +1,35 @@
 "use client";
 import React, { ReactNode } from "react";
 import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { schemas } from "@/schemas";
 
 export default function SignUpFormWrapper({
   children,
 }: {
   children: ReactNode;
 }) {
-  const formMethods = useForm();
-  console.log(name);
+  const formMethods = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      userName: "",
+    },
+    resolver: zodResolver(schemas.signUpSchema),
+  });
 
   return (
     <FormProvider {...formMethods}>
-      <form className=" flex flex-col gap-14">{children}</form>
+      <form
+        className=" flex flex-col gap-14"
+        onSubmit={formMethods.handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        {children}
+      </form>
     </FormProvider>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -15,19 +15,25 @@ export default function TextField({
   placeholder: string;
 }) {
   const formMethods = useFormContext();
+  console.log();
 
   return (
     <fieldset className=" flex flex-col gap-4">
       <label htmlFor={inputName} className=" text-20 font-normal">
         {inputLabel}
       </label>
-      <input
-        placeholder={placeholder}
-        type={inputType}
-        value={inputValue}
-        {...formMethods.register(inputName)}
-        className=" border w-96 rounded-5 border-light-grey-border placeholder:text-light-grey outline-none px-1 py-2 placeholder:capitalize"
-      />
+      <div>
+        <input
+          placeholder={placeholder}
+          type={inputType}
+          value={inputValue}
+          {...formMethods.register(inputName)}
+          className=" border w-96 rounded-5 border-light-grey-border placeholder:text-light-grey outline-none px-1 py-2 placeholder:capitalize"
+        />
+        <p className="text-red-bright">
+          *{formMethods?.formState?.errors[inputName]?.message as string}
+        </p>
+      </div>
     </fieldset>
   );
 }
