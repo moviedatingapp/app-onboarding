e629f0ebbd81e3652fb691ad7c5e06d6aced7521
sanskarchoudhry <sanskar22101@gmail.com>
2025-01-07 14:24:53 +0530

    api: implement sign-up api

--- src/app/(onboarding)/(sign-up)/sign-up/_components/SignUpFormWrapper.tsx
+++ src/app/(onboarding)/(sign-up)/sign-up/_components/SignUpFormWrapper.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { schemas } from "@/schemas";
+import axios from "axios";
 
 export default function SignUpFormWrapper({
   children,
@@ -20,14 +21,23 @@ export default function SignUpFormWrapper({
     resolver: zodResolver(schemas.signUpSchema),
   });
 
+  const handleSubmit = formMethods.handleSubmit(async (data) => {
+    await axios
+      .post("http://localhost:5000/sign-up", {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        userName: data.userName,
+        confirmPassword: data.confirmPassword,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  });
+
   return (
     <FormProvider {...formMethods}>
-      <form
-        className=" flex flex-col gap-14"
-        onSubmit={formMethods.handleSubmit((data) => {
-          console.log(data);
-        })}
-      >
+      <form className=" flex flex-col gap-14" onSubmit={handleSubmit}>
         {children}
       </form>
     </FormProvider>
